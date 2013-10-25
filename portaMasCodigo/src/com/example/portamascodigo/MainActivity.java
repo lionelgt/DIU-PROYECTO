d23fb6f92e8d3c7d0b26d5f07b7680076636c467
Carlos Geronimo <egosumcarlos@gmail.com>
2013-10-25 15:24:02 -0500

    a

--- portaMasCodigo/src/com/example/portamascodigo/MainActivity.java
+++ portaMasCodigo/src/com/example/portamascodigo/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.portamascodigo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ImageButton d=(ImageButton)findViewById(R.id.imageButton1);
+		d.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				Intent a= new Intent(getApplicationContext(),actividadDirectorio.class);
+				startActivity(a);
+				
+			}
+		});
 	}
 
 	@Override
